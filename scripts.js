3e93f5b24817092ff7b8cfbfe421ba002aed265e
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2022-10-31 10:10:36 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const num = Math.ceil(Math.random() * 100);
+moves = 10;
+
+function play() {
+    if (moves > 0) {
+        check();
+    } else {
+        reset();
+    }
+}
+
+function check() {
+    const guess = document.getElementById("guess").value;
+    const result = document.getElementById("result");
+    if (guess == num) {
+        result.innerHTML = "You guessed it!";
+    } else if (guess > num) {
+        result.innerHTML = "Too high!";
+        Move();
+    } else {
+        result.innerHTML = "Too low!";
+        Move();
+    }
+}
+
+function Move() {
+    moves--;
+    document.getElementById("moves").innerHTML = "0" + moves;
+    if (moves == 0) {
+        document.getElementById("result").innerHTML = "You lose!";
+    }
+}
+
+function reset() {
+    moves = 10;
+    document.getElementById("moves").innerHTML = moves;
+    document.getElementById("result").innerHTML = "Guess a number!";
+    num = Math.ceil(Math.random() * 100);
+}
